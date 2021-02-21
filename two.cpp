bfb6f6291ac34da17959a88ba5320ec6518704be
Tushar Saxena <stushar345@gmail.com>
2021-02-21 23:02:20 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s="bbaeia";
+    int k=4;
+
+    int count =0,maxi=0;
+    string str="";
+
+    if(k<=s.length())
+    {
+    for(int i=0;i<k;i++)
+    {
+        if(s[i]=='a'||s[i]=='e'||s[i]=='o'||s[i]=='u'||s[i]=='i')
+        count++;
+    }
+    str=s.substr(0,k);
+    maxi=count;
+    
+
+    int high=k;
+    for(int i=0;i<s.length()-k;i++)
+    {
+        if(s[i]=='a'||s[i]=='e'||s[i]=='o'||s[i]=='u'||s[i]=='i')
+        count--;
+        if(s[high]=='a'||s[high]=='e'||s[high]=='o'||s[high]=='u'||s[high]=='i')
+        count++;
+
+        if(count>maxi)
+        {
+            maxi=count;
+            str=s.substr(i+1,high);
+        }
+        high++;
+        
+    }
+    }
+    cout<<maxi<<"   "<<str;
+}
